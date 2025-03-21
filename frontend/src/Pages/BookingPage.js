62605b9415472a33dfbfdd745db046c09649d6b4
abhi-kr-210 <abhishekk1.ug21.ec@nitp.ac.in>
2025-03-21 15:40:59 +0530

    final commit

--- frontend/src/Pages/BookingPage.js
+++ frontend/src/Pages/BookingPage.js
@@ -1,46 +1,93 @@
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
-
-// Add your Mapbox access token here
-mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
-
+import React, { useState } from 'react'
+import "../Styles/BookingPage.css"
+import Modal from 'react-bootstrap/Modal';
 function BookingPage() {
-  const mapContainerRef = useRef(null);
-  const [map, setMap] = useState(null);
-
-  useEffect(() => {
-    const initializeMap = ({ setMap, mapContainerRef }) => {
-      const map = new mapboxgl.Map({
-        container: mapContainerRef.current,
-        style: 'mapbox://styles/mapbox/streets-v11', // Map style
-        center: [-74.5, 40], // Initial coordinates [longitude, latitude]
-        zoom: 9, // Initial zoom level
-      });
-
-      map.on('load', () => {
-        setMap(map);
-        map.resize();
-      });
-    };
-
-    if (!map) initializeMap({ setMap, mapContainerRef });
-
-    return () => map && map.remove();
-  }, [map]);
+  const [value, setValue] = useState('');
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const [bookname,setbookname]=useState('Normal')
   return (
-    <div>
-    {/* The map container */}
-    <div
-      ref={mapContainerRef}
-      style={{
-        width: '100%',
-        height: '500px',
-      }}
-    />
-  </div>
+    <section className='booking_page'>
+       <div className='location_input_container'>
+       <input type="text" value={value} placeholder='Enter your current location'
+       onChange={(e) => setValue(e.target.value)} />
+       </div>
+
+       
+       <div className='booking_options no-select'>
+         <div className='d-flex justify-content-between align-align-items-center normal'
+         onClick={() => setbookname('Normal')} style={{cursor:"pointer"}} >
+         <div className='d-flex justify-content-center align-items-center gap-3'>
+          <div >
+          <i className="bi bi-camera2" style={{fontSize:"2rem",color:"#00ceff"}}></i>
+          </div>
+          <p className='mb-0'>Normal</p>
+          <p className='mb-0'>4 mins</p>
+         </div>
+         <div className='d-flex justify-content-center align-items-center gap-2'>
+         <p className="mb-0"><i class="bi bi-currency-rupee"></i>350/hr</p>
+         <div  onClick={handleShow}><i class="bi bi-info-circle"></i></div>
+         </div>
+         </div>
+
+         <div className='d-flex justify-content-between align-align-items-center normal' 
+         onClick={() => setbookname('Standard')} style={{cursor:"pointer"}}>
+         <div className='d-flex justify-content-center align-items-center gap-3'>
+          <div >
+          <i className="bi bi-camera2" style={{fontSize:"2rem",color:"#00ceff"}}></i>
+          </div>
+          <p className='mb-0'>Standard </p>
+          <p className='mb-0'>4 mins</p>
+         </div>
+         <div className='d-flex justify-content-center align-items-center gap-2'>
+         <p className="mb-0"><i class="bi bi-currency-rupee"></i>550/hr</p>
+         <div><i class="bi bi-info-circle"></i></div>
+         </div>
+         </div>
+
+         <div className='d-flex justify-content-between align-align-items-center normal' 
+         onClick={() => setbookname('Advanced')} style={{cursor:"pointer"}}>
+         <div className='d-flex justify-content-center align-items-center gap-3'>
+          <div >
+          <i className="bi bi-camera2" style={{fontSize:"2rem",color:"#00ceff"}}></i>
+          </div>
+          <p className='mb-0'>Advanced </p>
+          <p className='mb-0'>4 mins</p>
+         </div>
+         <div className='d-flex justify-content-center align-items-center gap-2'>
+         <p className="mb-0"><i class="bi bi-currency-rupee"></i>750/hr</p>
+         <div><i class="bi bi-info-circle"></i></div>
+         </div>
+         </div>
+
+
+
+         <div className=' book_name_slot'>
+           <div className='d-flex justify-content-center align-items-center  book_name_slot_div'>
+             <p className='mb-0'>Book for {bookname} slot</p>
+           </div>
+          
+         </div>
+       </div>
+
+
+
+       <Modal show={show} onHide={handleClose} >
+       <Modal.Header closeButton  style={{color:"white",backgroundColor:"#00384e"}}>
+         <Modal.Title ><i className="bi bi-camera2 " ></i>  Normal Slot</Modal.Title>
+       </Modal.Header>
+       <Modal.Body>
+        <div></div>
+       </Modal.Body>
+       <Modal.Footer >
+       egwwwwwwwwwwwwwwwwwwww
+       </Modal.Footer>
+     </Modal>
+
+
+    </section>
   )
 }
 
 export default BookingPage
-
-
